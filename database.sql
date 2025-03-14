9969909b7ece75c1985f389e2dc355b7f2605578
AlexandreOsovski <alexandreosovski1@gmail.com>
2025-03-14 14:32:01 -0300

    fix: add arquivo database.sql

--- database.sql
+++ database.sql
@@ -28,62 +28,71 @@ VALUES
 DROP TABLE IF EXISTS cliente;
 
 CREATE TABLE
-    cliente (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        razao_social VARCHAR(255) NOT NULL,
-        cnpj VARCHAR(18) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        status_id INT,
-        UNIQUE (cnpj),
-        UNIQUE (email),
-        dataCriado DATETIME DEFAULT CURRENT_TIMESTAMP,
-        dataAlterado DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (status_id) REFERENCES status (id)
-    );
-
+    `cliente` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `razao_social` varchar(255) NOT NULL,
+        `cnpj` varchar(255) NOT NULL,
+        `email` varchar(255) NOT NULL,
+        `status_id` varchar(255) NOT NULL DEFAULT '1',
+        `dataCriado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `dataAlterado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 DROP TABLE IF EXISTS produto;
 
 CREATE TABLE
-    produto (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(255) NOT NULL,
-        descricao TEXT NOT NULL,
-        valor DECIMAL(10, 2) NOT NULL,
-        desconto DECIMAL(10, 2) DEFAULT NULL,
-        quantidade_estoque INT NOT NULL,
-        status_id INT,
-        dataCriado DATETIME DEFAULT CURRENT_TIMESTAMP,
-        dataAlterado DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (status_id) REFERENCES status (id)
-    );
-
+    `produto` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `nome` varchar(255) NOT NULL,
+        `descricao` varchar(255) NOT NULL,
+        `valor` decimal(10, 2) NOT NULL,
+        `desconto` decimal(10, 2) DEFAULT NULL,
+        `quantidade_estoque` int NOT NULL,
+        `status` varchar(255) NOT NULL DEFAULT 'ativo',
+        `dataCriado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `dataAlterado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 DROP TABLE IF EXISTS produto_imagem;
 
 CREATE TABLE
-    produto_imagem (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        idproduto INT NOT NULL,
-        url VARCHAR(255) NOT NULL,
-        dataCriado DATETIME DEFAULT CURRENT_TIMESTAMP,
-        dataAlterado DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (idproduto) REFERENCES produto (id)
-    );
-
+    `produto_imagem` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `url` varchar(255) NOT NULL,
+        `idproduto` int NOT NULL,
+        PRIMARY KEY (`id`),
+        KEY `FK_f17f4b3ec498e8d3f0d6bd548b2` (`idproduto`),
+        CONSTRAINT `FK_f17f4b3ec498e8d3f0d6bd548b2` FOREIGN KEY (`idproduto`) REFERENCES `produto` (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 DROP TABLE IF EXISTS pedido;
 
 CREATE TABLE
-    pedido (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        idcliente INT NOT NULL,
-        idproduto INT NOT NULL,
-        quantidade INT NOT NULL,
-        total DECIMAL(10, 2) NOT NULL,
-        total_desconto DECIMAL(10, 2) NOT NULL,
-        valor_unitario DECIMAL(10, 2) NOT NULL,
-        status_id INT,
-        dataCriado DATETIME DEFAULT CURRENT_TIMESTAMP,
-        dataAlterado DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (idcliente) REFERENCES cliente (id),
-        FOREIGN KEY (idproduto) REFERENCES produto (id),
-        FOREIGN KEY (status_id) REFERENCES status (id)
-    );
+    `pedido` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `status` varchar(255) NOT NULL DEFAULT 'pendente',
+        `dataCriado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `dataAlterado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        `id_cliente` int DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        KEY `FK_33471ba0a166506dbd244346ba6` (`id_cliente`),
+        CONSTRAINT `FK_33471ba0a166506dbd244346ba6` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 14 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+DROP TABLE IF EXISTS item_pedido;
+
+CREATE TABLE
+    `item_pedido` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `quantidade` int NOT NULL,
+        `valor_unitario` decimal(10, 2) NOT NULL,
+        `total` decimal(10, 2) NOT NULL,
+        `total_desconto` decimal(10, 2) NOT NULL,
+        `dataCriado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `dataAlterado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        `id_pedido` int DEFAULT NULL,
+        `id_produto` int DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        KEY `FK_b8a06a055ed50f426d61b625515` (`id_pedido`),
+        KEY `FK_24b16ca61542e380e35a21303cd` (`id_produto`),
+        CONSTRAINT `FK_24b16ca61542e380e35a21303cd` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`),
+        CONSTRAINT `FK_b8a06a055ed50f426d61b625515` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
